d1739d7cfaa8da031717403e692c557db21e35fe
Philip Cowperthwaite <pcowpert@cfa.harvard.edu>
2014-08-04 17:17:38 -0400

    First Working Version

--- grb.py
+++ grb.py
@@ -1,17 +1,35 @@
 # -*- python -*-
 
-import cgi, cookielib, datetime, logging, urllib2, webapp2
-#from google.appengine.ext import db
-#from google.appengine.api import taskqueue, urlfetch
+import cgi, cookielib, datetime, logging, urllib, urllib2, webapp2
+from google.appengine.ext import db
+from google.appengine.api import urlfetch
 
-class GetFeed (webapp2.RequestHandler):
+class ConfigRecord (db.Model):
+    name = db.StringProperty (required=True)
+    value = db.StringProperty (required=True)
+
+class SetKey (webapp2.RequestHandler):
     def get (self):
-        self.response.out.write ("Hello, world!")
+        key = self.request.get ('key')
+        self.response.out.write('set key to : ' + key)
+        ConfigRecord (name='api-key', value = key).put ()
 
+class GRB (webapp2.RequestHandler):
+    def get (self):
+        keys = list (db.GqlQuery ('SELECT * FROM ConfigRecord where name = \'api-key\''))
+        key = keys[0]
+        form_fields = {
+          "api_token": key.value,
+        }
+        form_data = urllib.urlencode(form_fields)
+        result = urlfetch.fetch(url="http://api.justyo.co/yoall/",
+            payload=form_data,
+            method=urlfetch.POST,
+            headers={'Content-Type': 'application/x-www-form-urlencoded'})
 
 handlers = [
-    ('/hello.txt', GetFeed)
-
+    ('/GRB.txt', GRB),
+    ('/admin/setkey', SetKey)
 ]
 
 app = webapp2.WSGIApplication (handlers)
